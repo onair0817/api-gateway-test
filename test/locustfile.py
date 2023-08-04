8895897fbbf3a9eff15ff2055ccdaa68d1dc8a64
Sungsoo <onair0817@gmail.com>
2023-08-04 10:47:19 +0900

    test: locust test code added

---
+++ test/locustfile.py
@@ -0,0 +1,10 @@
+# locustfile.py
+from locust import HttpUser, task, between
+
+
+class WebsiteUser(HttpUser):
+    wait_time = between(1, 2.5)
+
+    @task
+    def index(self):
+        self.client.get("/")
